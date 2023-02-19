ae5f2cba4d3c7864ab71c13710192d0e674acfe9
GuilhermeUrenha <guilherme.urenha@gmail.com>
2023-02-19 15:06:10 -0300

    misc rewrite

--- misc.js
+++ misc.js
@@ -2,25 +2,12 @@ function Time(Sec) { // / 1000
 	if (Sec / 60 >= 1) {
 		var Min = parseInt((Sec / 60), 10);
 		Sec %= 60;
-	}
-	else
-		if (Sec == Math.round(Sec))
-			return `${parseFloat(Sec)}s`;
-		else
-			return `${parseFloat(Sec).toFixed(2)}s`;
-
+	} else
+		return Sec == Math.round(Sec) ? `${parseFloat(Sec)}s` : `${parseFloat(Sec).toFixed(2)}s`;
 	if (Min / 60 >= 1) {
 		var Hr = parseInt((Min / 60), 10);
 		Min %= 60;
-	}
-	else
-		if (Sec == Math.round(Sec))
-			return `${Min}m ${parseFloat(Sec)}s`;
-		else
-			return `${Min}m ${parseFloat(Sec).toFixed(2)}s`;
-
-	if (Sec == Math.round(Sec))
-		return `${Hr}h ${Min}m ${parseFloat(Sec)}s`;
-	else
-		return `${Hr}h ${Min}m ${parseFloat(Sec).toFixed(2)}s`;
+	} else
+		return Sec == Math.round(Sec) ? `${Min}m ${parseFloat(Sec)}s` : `${Min}m ${parseFloat(Sec).toFixed(2)}s`;
+	return Sec == Math.round(Sec) ? `${Hr}h ${Min}m ${parseFloat(Sec)}s` : `${Hr}h ${Min}m ${parseFloat(Sec).toFixed(2)}s`;
 } exports.Time = Time;
