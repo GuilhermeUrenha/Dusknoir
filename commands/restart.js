a022204b23b145daa5ad8e04f4d761169241d349
GuilhermeUrenha <guilherme.urenha@gmail.com>
2023-02-23 03:17:55 -0300

    restart

--- commands/restart.js
+++ commands/restart.js
@@ -7,28 +7,9 @@ module.exports = {
 		.setName('restart')
 		.setDescription('Restart the client.'),
 	async execute(interaction) {
-		console.log('[Quitted.]');
-		interaction.reply('`[Quitted.]`');
+		console.log('[Exited.]');
+		await interaction.reply('`[Exited.]`');
 		await interaction.client.destroy();
 		process.exit(0);
-
-		/*
-        const execSync = require('child_process').execSync;
-        const output = execSync('Taskkill /IM node.exe /F && cd "C:\Users\guilh\Área de Trabalho\Info\BOTv2" && node index.js', {encoding: 'utf-8'});  // the default is 'buffer'
-        console.log('Output was:\n', output);
-
-        return await interaction.reply({content:`Output was:\n\`${output}\``, ephemeral:true});
-        */
-
-		/*
-        await interaction.reply('\`[Restarting.]\`');
-        global.setTimeout(async() =>{
-            interaction.client.login(process.env.token);
-            await interaction.editReply('\`[Restarted.]\`');
-            process
-        }, 5000);
-        await interaction.client.destroy();
-        process.exit();
-        */
 	}
 }
