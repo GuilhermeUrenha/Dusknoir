7896820eb00c507b86f11aa788a5937457c6b00c
GuilhermeUrenha <guilherme.urenha@gmail.com>
2023-02-24 22:53:04 -0300

    misc opt

--- misc.js
+++ misc.js
@@ -3,11 +3,11 @@ function Time(Sec) { // / 1000
 		var Min = parseInt((Sec / 60), 10);
 		Sec %= 60;
 	} else
-		return Sec == Math.round(Sec) ? `${parseFloat(Sec)}s` : `${parseFloat(Sec).toFixed(2)}s`;
+		return `${Sec == Math.round(Sec) ? parseFloat(Sec) : parseFloat(Sec).toFixed(2) }s`;
 	if (Min / 60 >= 1) {
 		var Hr = parseInt((Min / 60), 10);
 		Min %= 60;
 	} else
-		return Sec == Math.round(Sec) ? `${Min}m ${parseFloat(Sec)}s` : `${Min}m ${parseFloat(Sec).toFixed(2)}s`;
-	return Sec == Math.round(Sec) ? `${Hr}h ${Min}m ${parseFloat(Sec)}s` : `${Hr}h ${Min}m ${parseFloat(Sec).toFixed(2)}s`;
+		return `${Min}m ${Sec == Math.round(Sec) ? parseFloat(Sec) : parseFloat(Sec).toFixed(2) }s`;
+	return `${Hr}h ${Min}m ${Sec == Math.round(Sec) ? parseFloat(Sec) : parseFloat(Sec).toFixed(2)}s`;
 } exports.Time = Time;
